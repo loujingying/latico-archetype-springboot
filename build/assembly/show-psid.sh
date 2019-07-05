7eb56ac4ccb3f516d1d967b46259e2b8de6d656f
landingdong <landingdong@gdcattsoft.com>
2019-07-05 15:46:11 +0800

    查看进程信息增加显示程序路径

--- build/assembly/show-psid.sh
+++ build/assembly/show-psid.sh
@@ -7,5 +7,8 @@ PID=`ps -ef|grep $workdir|grep ${programname}|grep -v grep|awk '{print $2}'`
 if [ -z $PID ];then
     echo "The program <<$workdir && ${programname}>> has been stoped."
 else
-    echo $PID
+    echo "==========="
+    echo "|PID:$PID|"
+    echo "==========="
+    echo `ps -ef|grep $workdir|grep ${programname}|grep -v grep`
 fi
