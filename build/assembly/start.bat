4f76934b4d2055ba51b217032b1393e5cf4e02f8
landingdong <landingdong@gdcattsoft.com>
2019-07-24 23:47:46 +0800

    优化配置

--- build/assembly/start.bat
+++ build/assembly/start.bat
@@ -1,5 +1,5 @@
 set /p programname=<./programname.cfg
 set /p javapath=<./javapath.cfg
 title %programname%
-%javapath% -Dprogramname=%programname% -Dloader.path=lib -jar lib\%programname%.jar 2>err.log
+%javapath% -Dprogramname=%programname% -Dloader.path=lib -jar lib\%programname%.jar 2>logs/err.log
 pause
