a33c2b016d939075181fea9d6ccc9aea71b2cdb6
landingdong <landingdong@gdcattsoft.com>
2019-07-25 09:15:28 +0800

    优化配置

--- build/assembly/start.sh
+++ build/assembly/start.sh
@@ -8,6 +8,7 @@ PID=`ps -ef|grep $workdir|grep ${programname}|grep -v grep|awk '{print $2}'`
 
 if [ -z $PID ];then
     ${javapath} -Dprogramname=${programname} -Dprogrampath=$workdir -Dloader.path=lib -jar lib/${programname}.jar >/dev/null 2>logs/err.log &
+    sleep 1
     PID=`ps -ef|grep $workdir|grep ${programname}|grep -v grep|awk '{print $2}'`
     echo "PID=$PID the program <<$workdir && ${programname}>> starting..."
 else
