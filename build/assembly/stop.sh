cec020b15211c107315aec4bd4ec4ee327995d5c
landingdong <landingdong@gdcattsoft.com>
2019-07-27 16:57:49 +0800

    优化maven脚本

--- build/assembly/stop.sh
+++ build/assembly/stop.sh
@@ -11,10 +11,10 @@ else
     echo "Execute:kill ${PID}, the program <<${workdir} && ${programname}>> is being killed, please wait for 10 seconds..."
     sleep 10
 
-   PID=`ps -ef|grep ${workdir}|grep ${programname}|grep -v grep|awk '{print $2}'`
-    if [[ ${PID} ]];then
-        kill -9 ${PID}
-        echo "Execute:kill -9 ${PID}, the program <<${workdir} && ${programname}>> finished."
+   PID2=`ps -ef|grep ${workdir}|grep ${programname}|grep -v grep|awk '{print $2}'`
+    if [[ ${PID2} ]];then
+        kill -9 ${PID2}
+        echo "Execute:kill -9 ${PID2}, the program <<${workdir} && ${programname}>> finished."
     fi
     echo "Stop PID=${PID} the program <<${workdir} && ${programname}>> finished."
 fi
