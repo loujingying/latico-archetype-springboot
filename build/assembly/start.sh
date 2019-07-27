b46a4cf2d474230cdfe170d264c84d1c872e51fe
landingdong <landingdong@gdcattsoft.com>
2019-07-27 16:26:42 +0800

    优化启动脚本

--- build/assembly/start.sh
+++ build/assembly/start.sh
@@ -4,18 +4,19 @@ export programname=$(cat ./programname.cfg)
 export javapath=$(cat ./javapath.cfg)
 workdir=$(cd $(dirname $0); pwd)
 
-logdir="logs"
-if [ ! -d ${logdir} ];then
-  mkdir ${logdir}
-fi
+PID=`ps -ef|grep ${workdir}|grep ${programname}|grep -v grep|awk '{print $2}'`
 
-PID=`ps -ef|grep $workdir|grep ${programname}|grep -v grep|awk '{print $2}'`
+if [[ -z ${PID} ]];then
 
-if [ -z $PID ];then
-    ${javapath} -Dprogramname=${programname} -Dprogrampath=$workdir -Dloader.path=lib -jar lib/${programname}.jar >/dev/null 2>logs/err.log &
+    logdir="logs"
+    if [[ ! -d ${logdir} ]];then
+      mkdir ${logdir}
+    fi
+
+    ${javapath} -Dprogramname=${programname} -Dprogrampath=${workdir} -Dloader.path=lib -jar lib/${programname}.jar >/dev/null 2>logs/err.log &
     sleep 1
-    PID=`ps -ef|grep $workdir|grep ${programname}|grep -v grep|awk '{print $2}'`
-    echo "PID=$PID the program <<$workdir && ${programname}>> starting..."
+    PID=`ps -ef|grep ${workdir}|grep ${programname}|grep -v grep|awk '{print $2}'`
+    echo "PID=$PID the program <<${workdir} && ${programname}>> starting..."
 else
-    echo "PID=$PID the program <<$workdir && ${programname}>> has been running.Please stop it firstly."
+    echo "PID=$PID the program <<${workdir} && ${programname}>> has been running.Please stop it firstly."
 fi
