5c67dc442130dea2d72722424015a32fa7fed443
landingdong <landingdong@gdcattsoft.com>
2019-12-30 18:13:34 +0800

    增加pinpoint配置

---
+++ build/assembly/start-with-pp.bat
@@ -0,0 +1,9 @@
+set /p programname=<./programname.cfg
+set /p javapath=<./javapath-with-pp.cfg
+title %programname%
+
+set logdir="logs"
+IF NOT EXIST %logdir% mkdir %logdir%
+
+%javapath% -Dprogramname=%programname% -Dloader.path=lib -jar lib\%programname%.jar 2>logs/err.log
+pause
---
+++ build/assembly/start-with-pp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# ${project.build.finalName}
+export programname=$(cat ./programname.cfg)
+export javapath=$(cat ./javapath-with-pp.cfg)
+workdir=$(cd $(dirname $0); pwd)
+
+PID=`ps -ef|grep ${workdir}|grep ${programname}|grep -v grep|awk '{print $2}'`
+
+if [[ -z ${PID} ]];then
+
+    logdir="logs"
+    if [[ ! -d ${logdir} ]];then
+      mkdir ${logdir}
+    fi
+
+    ${javapath} -Dprogramname=${programname} -Dprogrampath=${workdir} -Dloader.path=lib -jar lib/${programname}.jar >/dev/null 2>logs/err.log &
+    sleep 1
+    PID=`ps -ef|grep ${workdir}|grep ${programname}|grep -v grep|awk '{print $2}'`
+    echo "PID=$PID the program <<${workdir} && ${programname}>> starting..."
+else
+    echo "PID=$PID the program <<${workdir} && ${programname}>> has been running.Please stop it firstly."
+fi
