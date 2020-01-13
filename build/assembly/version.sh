c4e11de1f930bd0d8461bda44f71ca5711212f97
landingdong <landingdong@gdcattsoft.com>
2020-01-13 23:12:54 +0800

    查看版本号的脚本自动读取端口

--- build/assembly/version.sh
+++ build/assembly/version.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#要访问的IP和端口
+#要访问的IP、端口、上下文路径，上下文路径的前缀是/开头，后缀不带/
 ip=127.0.0.1
 port=8080
+contextPath=/
 
 # 读取application.properties配置文件中的端口
 for line in `cat ./config/application.properties`
@@ -9,14 +10,18 @@ do
     if [[ ${line} == appPort=* ]]
     then
         port=${line:8}
-        break
+
+    elif [[ ${line} == appContextPath=* ]]
+    then
+        contextPath=${line:15}
+
     fi
 done
 
 echo "------------------------------------------------------"
-echo "curl -i --max-time 10 \"http://${ip}:${port}/version\""
+echo "curl -i --max-time 10 \"http://${ip}:${port}${contextPath}/version\""
 echo "------------------------------------------------------"
 # 执行http访问
-curl -i --max-time 10 "http://${ip}:${port}/version"
+curl -i --max-time 10 "http://${ip}:${port}${contextPath}/version"
 echo ""
 echo "------------------------------------------------------"
