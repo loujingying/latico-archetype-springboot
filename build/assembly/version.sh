7d05f5cb503cec058ef22aeaa131902f9b28c1b2
landingdong <landingdong@gdcattsoft.com>
2020-01-13 23:01:48 +0800

    查看版本号的脚本自动读取端口

--- build/assembly/version.sh
+++ build/assembly/version.sh
@@ -3,5 +3,20 @@
 ip=127.0.0.1
 port=8080
 
+# 读取application.properties配置文件中的端口
+for line in `cat ./config/application.properties`
+do
+    if [[ ${line} == appPort=* ]]
+    then
+        port=${line:8}
+        break
+    fi
+done
+
+echo "------------------------------------------------------"
+echo "curl -i --max-time 10 \"http://${ip}:${port}/version\""
+echo "------------------------------------------------------"
 # 执行http访问
 curl -i --max-time 10 "http://${ip}:${port}/version"
+echo ""
+echo "------------------------------------------------------"
