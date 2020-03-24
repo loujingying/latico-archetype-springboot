290ea1049e6ac82573b6718f65baf04e1987e9f1
landingdong <landingdong@gdcattsoft.com>
2020-03-24 17:21:16 +0800

    1、集成hadoop

--- src/test/java/com/latico/archetype/springboot/hadoop/HdfsUtilsTest.java
+++ src/test/java/com/latico/archetype/springboot/hadoop/HdfsUtilsTest.java
@@ -7,16 +7,35 @@ import static org.junit.Assert.*;
 
 public class HdfsUtilsTest {
 
-    FileSystem fileSystem = null;
+
     /**
      *
      */
     @Test
-    public void test(){
-
+    public void mkdir(){
+        FileSystem fileSystem = null;
         try {
-            fileSystem = HdfsUtils.newInstanceFileSystem("hdfs://localhost:9000", "latico");
-            System.out.println(HdfsUtils.existPath(fileSystem, "/abc"));
+            fileSystem = HdfsUtils.newInstanceFileSystem("hdfs://192.168.101.103:9000", "hadoop");
+            String dir = "/demo";
+            System.out.println("创建目录:" + HdfsUtils.mkdir(fileSystem, dir));
+            System.out.println("是否存在目录:" + HdfsUtils.existPath(fileSystem, dir));
+            System.out.println("删除目录:" + HdfsUtils.deleteFile(fileSystem, dir));
+            System.out.println("是否存在目录:" + HdfsUtils.existPath(fileSystem, dir));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            HdfsUtils.close(fileSystem);
+        }
+    }
+
+    @Test
+    public void test(){
+        FileSystem fileSystem = null;
+        try {
+            fileSystem = HdfsUtils.newInstanceFileSystem("hdfs://192.168.101.103:9000", "hadoop");
+            String dir = "/demo";
+            System.out.println(HdfsUtils.mkdir(fileSystem, dir));
+            System.out.println("是否存在目录:" + HdfsUtils.existPath(fileSystem, dir));
         } catch (Exception e) {
             e.printStackTrace();
         }finally {
